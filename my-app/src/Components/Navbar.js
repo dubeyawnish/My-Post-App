c182163014448bf638586a147bb25247718a33e6
Awnish Dubey <dubeyawnish6@gmail.com>
2023-04-04 01:43:03 +0530

    contact page is responsive

--- my-app/src/Components/Navbar.js
+++ my-app/src/Components/Navbar.js
@@ -25,7 +25,7 @@ const Navbar = () => {
                         <form className="d-flex" role="search">
                             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
                             <button className="btn btn-primary" type="submit">
-                            <i class="fa-solid fa-magnifying-glass"></i>
+                            <i className="fa-solid fa-magnifying-glass"></i>
                             </button>
                         </form>
                     </div>
--- my-app/src/Components/Screen/Contact.js
+++ my-app/src/Components/Screen/Contact.js
@@ -1,38 +1,39 @@
 import React from 'react'
+import './Contact.css'
 
 const Contact = () => {
     return (
         <div className="container">
             <h3 className="text-center text-uppercase pt-4">Contact Us</h3>
-            <div className="text-muted shadow-sm p-3 lh-2" >
+            <div className="contact-form-container mx-auto text-muted shadow-sm p-3 lh-2" >
 
                 <form>
                     <div className="mb-3">
-                        <label for="name" className="form-label">Fullname</label>
+                        <label htmlFor="name" className="form-label">Fullname</label>
                         <input type="text" className="form-control" id="name" required />
                     </div>
                     <div className="mb-3">
-                        <label for="phone" className="form-label">Contact no.</label>
+                        <label htmlFor="phone" className="form-label">Contact no.</label>
                         <input type="text" className="form-control" id="phone" required />
                     </div>
 
                     <div className="mb-3">
-                        <label for="email" className="form-label">Email address</label>
+                        <label htmlFor="email" className="form-label">Email address</label>
                         <input type="email" className="form-control" id="email" aria-describedby="emailHelp" required />
                         <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                     </div>
                     <div className='mb-3'>
-                        <label for="timing" className="form-label">Where can we reach you?</label>
+                        <label htmlFor="timing" className="form-label">Where can we reach you?</label>
 
                         <select className="form-select" aria-label="Default select example" id="timing">
-                            <option selected>Best Time To Reach</option>
+                            <option defaultValue="">Best Time To Reach</option>
                             <option value="M">Morning</option>
                             <option value="A">Afternoon</option>
                             <option value="E">Evening</option>
                         </select>
                     </div>
                     <div className="mb-3">
-                        <label for="query" className="form-label">Enter your query below</label>
+                        <label htmlFor="query" className="form-label">Enter your query below</label>
                         <textarea id="query" className="form-control" required></textarea>
                     </div>
 
