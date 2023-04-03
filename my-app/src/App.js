e0bc007f9ec0626515b1609828b2a3f3902c42a2
Awnish Dubey <dubeyawnish6@gmail.com>
2023-04-03 23:51:41 +0530

    navbar done

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Navbar from './Components/Navbar';
+import About from './Components/Screen/About';
+import Contact from './Components/Screen/Contact';
+
+function App() {
+  return (
+    <div className="">
+     <Navbar />
+    <About />
+    <Contact />
+      
+    </div>
+  );
+}
+
+export default App;
