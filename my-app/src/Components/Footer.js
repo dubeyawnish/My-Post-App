fb1dfb32f1ee44b9eaf8c2226342dcec2b187a85
Awnish Dubey <dubeyawnish6@gmail.com>
2023-04-04 09:03:15 +0530

    Designing footer Newsletter

--- my-app/src/Components/Footer.js
+++ my-app/src/Components/Footer.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className="  container-fluid text-white bg-primary bg-gradient " style={{ minHeight: '12rem' }}>
-            <div className="row pt-2 text-center">
+        <div className=" mt-5 container-fluid text-white bg-primary bg-gradient " style={{ minHeight: '12rem' }}>
+            <div className="row pt-4 text-center">
                 {/* created 3 section for large and medium devices it takes 4-col each but for small devices it takes 12 col */}
                 <div className="col-lg-4 col-md-4 col-sm-12 ">
                     <h5>Quick Link</h5>
-                    <div className="d-flex flex-column border border-danger">
-                        <a  className="text-white p-1" href="">About Us</a>
+                    <div className="d-flex flex-column ">
+                        <a className="text-white p-1" href="">About Us</a>
                         <a className="text-white p-1" href="">Our Post</a>
                         <a className="text-white p-1" href="">Our Team</a>
                         <a className="text-white p-1" href="">Contact Us</a>
@@ -19,6 +19,11 @@ const Footer = () => {
                 </div>
                 <div className="col-lg-4 col-md-4 col-sm-12 ">
                     <h5>Newsletter</h5>
+                    <div className="d-flex flex-column justify-content-around " style={{ minHeight: "8rem" }}>
+                        <input type="text" className="form-control" placeholder='Subscribe' />
+                        <button className="btn btn-warning text-white">Subscribe</button>
+
+                    </div>
 
                 </div>
                 <div className="col-lg-4 col-md-4 col-sm-12 ">
