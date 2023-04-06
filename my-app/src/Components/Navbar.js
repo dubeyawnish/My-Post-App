f12e4270d4cdd22758998f9e3086c499d51fda7b
Awnish Dubey <dubeyawnish6@gmail.com>
2023-04-06 12:04:45 +0530

    login and logout done

--- my-app/src/Components/Navbar.js
+++ my-app/src/Components/Navbar.js
@@ -1,17 +1,17 @@
 import React from 'react'
-import {NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import { UserContext } from '../App';
 import { useContext } from 'react';
 
 const Navbar = () => {
-    const {state,dispatch}=useContext(UserContext);
-    const navigate= useNavigate();
+    const { state, dispatch } = useContext(UserContext);
+    const navigate = useNavigate();
 
-    const logout =(eve)=>{
+    const logout = (eve) => {
         eve.preventDefault();
         localStorage.clear();
-        dispatch({type:'LOGOUT'});
+        dispatch({ type: 'LOGOUT' });
         navigate('/login')
 
     }
@@ -27,19 +27,24 @@ const Navbar = () => {
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
+                            <li className="nav-item">
                                 <NavLink className="nav-link text-uppercase" to="/">Home</NavLink>
                             </li>
                             <li className="nav-item">
                                 <NavLink className="nav-link text-uppercase" to="/posts">Post</NavLink>
                             </li>
-                            <li className="nav-item">
-                                <NavLink className="nav-link text-uppercase" to="/create">Create Post</NavLink>
-                            </li>
+                            {
+                                state ?
+
+                                    <li className="nav-item">
+                                        <NavLink className="nav-link text-uppercase" to="/create">Create Post</NavLink>
+                                    </li>
+                                    : ""
+                            }
                             <li className="nav-item">
                                 <NavLink className="nav-link text-uppercase" aria-current="page" to="/about">AboutUs</NavLink>
                             </li>
-                            
+
                             <li className="nav-item">
                                 <NavLink className="nav-link text-uppercase" to="/contact">Contact</NavLink>
                             </li>
@@ -49,19 +54,29 @@ const Navbar = () => {
                         <form className="d-flex" role="search">
                             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
                             <button className="btn btn-primary" type="submit">
-                            <i className="fa-solid fa-magnifying-glass"></i>
+                                <i className="fa-solid fa-magnifying-glass"></i>
                             </button>
 
                         </form>
-                        <li className="nav-item">
-                                <NavLink className=" btn btn-primary  " to="/signup"><i className="fa-solid fa-user-plus"></i></NavLink>
-                        </li>
-                        <li className="nav-item">
-                                <NavLink className=" btn btn-primary " to="/login"><i className="fa-solid fa-right-to-bracket"></i></NavLink>
-                        </li>
-                        <li className="nav-item">
-                                <button onClick={(eve)=>{logout(eve)}} className=" btn btn-danger " ><i className="fa-solid fa-right-to-bracket"></i></button>
-                        </li>
+
+                        {
+                            state ?
+                                <li className="nav-item">
+                                    <button onClick={(eve) => { logout(eve) }} className=" btn btn-danger " ><i className="fa-solid fa-right-to-bracket"></i></button>
+                                </li>
+                                :
+                                <>
+
+                                    <li className="nav-item">
+                                        <NavLink className=" btn btn-primary " to="/login"><i className="fa-solid fa-right-to-bracket"></i></NavLink>
+                                    </li>
+                                    <li className="nav-item">
+                                        <NavLink className=" btn btn-primary  " to="/signup"><i className="fa-solid fa-user-plus"></i></NavLink>
+                                    </li>
+                                </>
+
+                        }
+
                     </div>
                 </div>
             </nav>
